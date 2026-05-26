9133e6d1aee7ac7e75961667ed0830cdf1886b8e
bociwess0 <bogdandjordjevic2000@gmail.com>
2026-05-26 22:03:04 +0200

    starting with header - adding logo and styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import Header from "./layout/Header"
+
 function App() {
   return (
-    <div className="min-h-screen bg-[#111827] text-white flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-violet-400">
+    <div className="min-h-screen bg-[#111827]">
+      <Header />
+      {/* <h1 className="text-5xl font-bold text-violet-400">
         YourPlaces
-      </h1>
+      </h1> */}
     </div>
   )
 }
---
+++ frontend/src/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from '../assets/logo.svg';
+
+
+export default function Header() {
+  return (
+    <div className="sticky top-0 left-0 bg-[#040B1A]">
+      <img src={logo} alt="YourPlaces Logo" />
+    </div>
+  );
+}
