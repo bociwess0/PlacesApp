58aca45addb102088c0fced4ae53997579635b7f
bociwess0 <bogdandjordjevic2000@gmail.com>
2026-05-29 17:46:57 +0200

    fixing image urls again

--- frontend/src/data/all-data.ts
+++ frontend/src/data/all-data.ts
@@ -1,8 +1,8 @@
 import type { User } from "../types";
 
 export const allUsers: User[] = [
-    { id: 1, name: "Michael", image: "public/images/profile_image_1.png", places: 1 },
-    { id: 2, name: "Nicolas", image: "public/images/profile_image_1.png", places: 0 },
-    { id: 3, name: "Fernando", image: "public/images/profile_image_1.png", places: 1 },
-    { id: 4, name: "Mason", image: "public/images/profile_image_1.png", places: 3 }
+    { id: 1, name: "Michael", image: "images/profile_image_1.png", places: 1 },
+    { id: 2, name: "Nicolas", image: "images/profile_image_1.png", places: 0 },
+    { id: 3, name: "Fernando", image: "images/profile_image_1.png", places: 1 },
+    { id: 4, name: "Mason", image: "images/profile_image_1.png", places: 3 }
 ];
